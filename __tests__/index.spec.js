25fc627fb8f981cb46b3189f0520caba9ba0892e
Michał Pietrucha <mpietrucha50@gmail.com>
2025-02-10 12:53:30 +0100

    bump

--- __tests__/index.spec.js
+++
@@ -1,6 +0,0 @@
-import config from '@'
-import { expect, test } from 'vitest'
-
-test('package index returns prettier config object', () => {
-    expect(config).toBeTypeOf('object')
-})
--- vitest.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vitest/config'
-import { alias } from './build.config.js'
-
-export default defineConfig({
-    resolve: { alias },
-})
